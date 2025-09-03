23d0967d3b65b9bf05031fc520f95b7d12751b27
Laércio Fernandes Correia de Melo Neto <laerciofernandesmelonetoo@gmail.com>
2025-09-03 19:38:25 -0300

    Refactor data extraction and loading process; implement database setup in loady.py

--- src/extract.py
+++ src/extract.py
@@ -1,15 +1,18 @@
 import requests
-import sqlite3
 
 
 class extract:
 
     def __init__(self):
         pass
-    
-url = "http://universities.hipolabs.com/search?country=Brazil"
 
-# Acessando o link da internet
-response = requests.get(url)
-response.raise_for_status()  
-universities = response.json()
+    def extract_data(self, country):
+
+        url = f"http://universities.hipolabs.com/search?country={country}"
+
+        # Acessando o link da internet
+        response = requests.get(url)
+        response.raise_for_status()  
+        universities = response.json()
+
+        return universities
