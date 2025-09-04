f5081cd832e667debe765d8d0485ca178b962abc
Laércio Fernandes Correia de Melo Neto <laerciofernandesmelonetoo@gmail.com>
2025-09-03 21:23:23 -0300

    Refactor code structure for improved readability and maintainability

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 from src.extract import extract
+from src.load import load
 
 
 Extract = extract()
+Load = load()
 
-print(Extract.extract_data("Brazil"))
+br = Extract.extract_data("Brazil")
+Load.load_data("universities", "universities_br", br)
+
+it = Extract.extract_data("Italy")
+Load.load_data("universities", "universities_it", it)
--- src/extract.py
+++ src/extract.py
@@ -6,11 +6,17 @@ class extract:
     def __init__(self):
         pass
 
-    def extract_data(self, country):
+    def extract_data(self, country: str) -> list[dict]:
+        """
+        Método responsável por extrair os dados da API e transformar em uma lista com dicionários.
+        
+        Args:
+            country: str - Nome do país que será pesquisado na API.
+
+        """
 
         url = f"http://universities.hipolabs.com/search?country={country}"
 
-        # Acessando o link da internet
         response = requests.get(url)
         response.raise_for_status()  
         universities = response.json()
